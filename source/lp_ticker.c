a0fa76fdd5e23e42496ed12f46c9ea4751e46dc9
Wolfgang Betz <wolfgang.betz@st.com>
2015-10-22 13:08:31 +0200

    Move 'lp_ticker.c' back to this module Use a more generic approach for tick frequency calculation Should work as long as TIM2 is on APB1

---
+++ source/lp_ticker.c
@@ -0,0 +1,116 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2015 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "cmsis.h"
+#include "device.h"
+
+#if DEVICE_LOWPOWERTIMER
+
+#include "lp_ticker_api.h"
+#include "sleep_api.h"
+#include "uvisor-lib/uvisor-lib.h"
+
+static TIM_HandleTypeDef TimMasterHandle;
+static uint8_t lp_ticker_inited = 0;
+static volatile uint32_t overflows = 0;
+
+static void lp_handler(void)
+{
+    __HAL_TIM_CLEAR_IT(&TimMasterHandle, TIM_IT_CC1);
+    overflows++;
+}
+
+void lp_ticker_init(void) {
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    uint32_t TimClk;
+
+    if (!lp_ticker_inited) {
+        lp_ticker_inited = 1;
+        __TIM2_CLK_ENABLE();
+        __TIM2_FORCE_RESET();
+        __TIM2_RELEASE_RESET();
+	
+	// Get clock configuration
+	// Note: TimClk contains here the Latency (not used after)
+	HAL_RCC_GetClockConfig(&RCC_ClkInitStruct, &TimClk);
+	
+	// Get TIM2 clock value
+	TimClk = HAL_RCC_GetPCLK1Freq();
+  
+	// TIMxCLK = PCLKx when the APB prescaler = 1 else TIMxCLK = 2 * PCLKx
+	if (RCC_ClkInitStruct.APB1CLKDivider == RCC_HCLK_DIV2)
+		TimClk *= 2;
+  
+        // Configure time base
+        TimMasterHandle.Instance = TIM2;
+        TimMasterHandle.Init.Period            = 0xFFFFFFFF;
+	if((uint32_t)(TimClk / 1000) <= 0xFFFFU) {
+		/* betzw: having a 16-bit prescaler, beyond settings work only for timer clock 
+		          frequencies below 65.536.000Hz!!! */
+		TimMasterHandle.Init.Prescaler = (uint32_t)(TimClk / 1000) - 1; // 1 ms tick
+	} else if((uint32_t)(TimClk / 2000) <= 0xFFFFU) {
+ 		/* betzw: having a 16-bit prescaler, beyond settings work only for timer clock 
+		          frequencies below 131.072.000Hz!!! */
+		TimMasterHandle.Init.Prescaler = (uint32_t)(TimClk / 2000) - 1; // 0.5 ms tick
+       } else {
+		/* betzw: lowest possible frequency, i.e. (TimClk / 65536)
+		          which e.g. means for a timer clock of 84.000.000Hz a 
+                          counter frequency of ~1282Hz, i.e. a ~780 us tick */
+		TimMasterHandle.Init.Prescaler = 0xFFFFU;
+        }
+        TimMasterHandle.Init.ClockDivision     = 0;
+        TimMasterHandle.Init.CounterMode       = TIM_COUNTERMODE_UP;
+        TimMasterHandle.Init.RepetitionCounter = 0;
+        HAL_TIM_OC_Init(&TimMasterHandle);
+
+        vIRQ_SetVector(TIM2_IRQn, (uint32_t)lp_handler);
+        vIRQ_EnableIRQ(TIM2_IRQn);
+
+        HAL_TIM_OC_Start(&TimMasterHandle, TIM_CHANNEL_1);
+    }
+}
+
+uint32_t lp_ticker_read() {
+    if (!lp_ticker_inited) {
+        lp_ticker_init();
+    }
+    return TIM2->CNT;
+}
+
+uint32_t lp_ticker_get_overflows_counter(void) {
+    return overflows;
+}
+
+uint32_t lp_ticker_get_compare_match(void) {
+    return TIM2->CCMR1;
+}
+
+void lp_ticker_set_interrupt(uint32_t now, uint32_t time) {
+    (void)now;
+    // Set new output compare value
+    __HAL_TIM_SetCompare(&TimMasterHandle, TIM_CHANNEL_1, time);
+    // Enable IT
+    __HAL_TIM_ENABLE_IT(&TimMasterHandle, TIM_IT_CC1);
+}
+
+void lp_ticker_sleep_until(uint32_t now, uint32_t time)
+{
+    lp_ticker_set_interrupt(now, time);
+    sleep_t sleep_obj;
+    mbed_enter_sleep(&sleep_obj);
+    mbed_exit_sleep(&sleep_obj);
+}
+
+#endif
